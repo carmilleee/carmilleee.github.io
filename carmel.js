3ad830228a204e1e2a225b20948772e57104f3cf
carmilleee <carmillefashion@gmail.com>
2015-07-24 14:35:34 -0700

    first comment

---
+++ carmel.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+$("p").hide();
+
+})
+
+
+
+
+
+
+
+
+
+/*var imagesArray = newArray("pics/n.png","pics/m.png","pics/i.png");
+
+/*function displayImage(){
+    var num = Math.floor(Math.random() * (imagesArray.length+1));  // 0...6
+    document.canvas.src ='http://placehold.it/150x150/cf5&text=' + imagesArray[num];
+};*/
+
+/*function getRandomImage() { 
+var randNum = Math.floor(Math.random()*imagesArray.length);
+document.getElementById("myPicture") src = imagesArray[randomNum];
+};*/
+
+/*someimage.src=imagesArray(Math.floor(Math.random()*imagesArray.length));*/
+
+
+
+
+
+    
+    
+    
+    
